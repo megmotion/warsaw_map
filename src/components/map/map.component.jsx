d0b568ae81ed78f1dcaacc65c374ac851541b3ff
megmotion <33497517+megmotion@users.noreply.github.com>
2019-08-18 01:00:15 +0200

    create map

--- src/components/map/map.component.jsx
+++ src/components/map/map.component.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React from 'react'
+import mapboxgl from 'mapbox-gl'
 
-const WarsawMap  = () => {
-	return(
-		<h1>Map</h1>
-	)
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+
+class WarsawMap extends React.Component {
+	componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [21, 52.25],
+      zoom: 12
+    });
+  }
+  componentWillUnmount() {
+    this.map.remove();
+  }
+  render() {
+    return (
+    	<div>
+    		<div style={{position:'absolute', top:0, bottom:0, width:'100%'}} ref={el => this.mapContainer = el} />
+    	</div>
+    );
+  }
 }
 export default WarsawMap;
