19ab38e44863625db0945db7dcfda9b1a5fa5a6f
megmotion <33497517+megmotion@users.noreply.github.com>
2019-08-18 08:59:48 +0200

    add cinemas data

---
+++ src/cinemas.js
@@ -0,0 +1,87 @@
+const cinemas = [
+		{
+			"type": "Feature",
+			"properties": {
+			"title": "Kino Praha",
+			"description": "http://www.kinopraha.pl/"
+			},
+			"geometry": {
+			"coordinates": [
+			21.035452,
+			52.252933
+			],
+			"type": "Point"
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+			"title": "Kino Wisła",
+			"description": "http://www.novekino.pl/kina/wisla/index.php"
+			},
+			"geometry": {
+			"coordinates": [
+			20.986701,
+			52.270374
+			],
+			"type": "Point"
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+			"title": "Kinoteka",
+			"description": "http://www.kinoteka.pl/"
+			},
+			"geometry": {
+			"coordinates": [
+			21.006524,
+			52.232383
+			],
+			"type": "Point"
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+			"title": "Kino Luna",
+			"description": "http://www.kinoluna.pl/"
+			},
+			"geometry": {
+			"coordinates": [
+			21.019658,
+			52.219659
+			],
+			"type": "Point"
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+			"title": "Kino Muranów",
+			"description": "http://kinomuranow.pl/"
+			},
+			"geometry": {
+			"coordinates": [
+			20.999576,
+			52.246313
+			],
+			"type": "Point"
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+			"title": "Kino Iluzjon",
+			"description": "http://www.iluzjon.fn.org.pl/"
+			},
+			"geometry": {
+			"coordinates": [
+			21.011023,
+			52.205806
+			],
+			"type": "Point"
+			}
+		}
+		]
+export default cinemas;
