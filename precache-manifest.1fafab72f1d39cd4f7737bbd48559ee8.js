6fabbb294c2984ce7793d762ab853f02d041082e
megmotion <33497517+megmotion@users.noreply.github.com>
2019-08-23 03:36:07 +0200

    Updates

---
+++ precache-manifest.1fafab72f1d39cd4f7737bbd48559ee8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecdfbe1a43bafc59bd9e815874314dec",
+    "url": "/warsaw_map/index.html"
+  },
+  {
+    "revision": "329e68a34c836176262a",
+    "url": "/warsaw_map/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ebdeeb47f356c4d348d8",
+    "url": "/warsaw_map/static/js/2.1acaa2ce.chunk.js"
+  },
+  {
+    "revision": "329e68a34c836176262a",
+    "url": "/warsaw_map/static/js/main.d9e84aa1.chunk.js"
+  },
+  {
+    "revision": "77dfe570a1b98ffa169a",
+    "url": "/warsaw_map/static/js/runtime~main.e6b3dd9f.js"
+  }
+]);
