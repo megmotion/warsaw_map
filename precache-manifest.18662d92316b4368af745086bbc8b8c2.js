0d11f67dbc70002429364aab6a1d9d7d0e98d3de
megmotion <33497517+megmotion@users.noreply.github.com>
2019-08-25 14:49:08 +0200

    Updates

--- precache-manifest.18662d92316b4368af745086bbc8b8c2.js
+++ precache-manifest.18662d92316b4368af745086bbc8b8c2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e5be37c94bbe14232bc3265ff2c617a0",
+    "revision": "36595a9f3cd495b480f7a425df95e02b",
     "url": "/warsaw_map/index.html"
   },
   {
-    "revision": "e2ac9a8c2dd10fe6ef39",
+    "revision": "26527c360af16f5830ad",
     "url": "/warsaw_map/static/css/main.34de6062.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/warsaw_map/static/js/2.1acaa2ce.chunk.js"
   },
   {
-    "revision": "e2ac9a8c2dd10fe6ef39",
-    "url": "/warsaw_map/static/js/main.63de6d6a.chunk.js"
+    "revision": "26527c360af16f5830ad",
+    "url": "/warsaw_map/static/js/main.1e0f6ab2.chunk.js"
   },
   {
     "revision": "77dfe570a1b98ffa169a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/warsaw_map/precache-manifest.95bf63610a369bf781bed930386d3648.js"
+  "/warsaw_map/precache-manifest.18662d92316b4368af745086bbc8b8c2.js"
 );
 
 self.addEventListener('message', (event) => {
